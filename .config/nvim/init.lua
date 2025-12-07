ac8c3592a2808d16a6719a09ce08aabc22320e0a
toketas <nishizuka23@gmail.com>
2025-12-06 23:35:42 -0300

    feat: update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,164 @@
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.cursorline = true
+vim.opt.mouse = "a"
+vim.opt.wildmenu = true
+vim.opt.swapfile = false
+
+-- Set tabwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("vim-settings")
-require("lazy").setup("plugins")
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+local plugins = {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "nvim-treesitter/nvim-treesitter", branch = "master", lazy = false, build = ":TSUpdate" },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "v0.2.0",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons", -- optional, but recommended
+		},
+		lazy = false, -- neo-tree will lazily load itself
+	},
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			animate = { enabled = false },
+			bigfile = { enabled = true },
+			dashboard = { enabled = true },
+			explorer = { enabled = false },
+			indent = { enabled = true },
+			input = { enabled = true },
+			picker = { enabled = true },
+			notifier = { enabled = true },
+			quickfile = { enabled = true },
+			scope = { enabled = true },
+			scroll = { enabled = true },
+			statuscolumn = { enabled = true },
+			words = { enabled = true },
+		},
+		keys = {
+			{
+				"<c-p>",
+				function()
+					Snacks.picker("files")
+				end,
+				desc = "Find files",
+			},
+			{
+				"<c-i>",
+				function()
+					Snacks.picker()
+				end,
+				desc = "Find code",
+			},
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
+}
+
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = plugins,
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+})
+
+require("nvim-treesitter.configs").setup({
+	ensure_installed = {
+		"c",
+		"lua",
+		"vim",
+		"markdown",
+		"yaml",
+		"dockerfile",
+		"python",
+		"terraform",
+		"sql",
+		"csv",
+		"bash",
+		"ssh_config",
+	},
+	highlight = {
+		enable = true,
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+})
+
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "isort", "black" },
+		-- You can customize some of the format options for the filetype (:help conform.format)
+		rust = { "rustfmt", lsp_format = "fallback" },
+		-- Conform will run the first available formatter
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+		yaml = { "yamlfix" },
+	},
+})
+
+vim.cmd.colorscheme("catppuccin")
+vim.api.nvim_set_option("clipboard", "unnamedplus")
+vim.g.snacks_animate = false
+-- keymaps
+vim.keymap.set("n", "<C-n>", "<Cmd>Neotree toggle<CR>", { silent = true })
+vim.keymap.set("n", "<C-Left>", "<Cmd>tabprevious<CR>", { silent = true })
+vim.keymap.set("n", "<C-Right>", "<Cmd>tabnext<CR>", { silent = true })
+vim.keymap.set("n", "<C-t>", "<Cmd>tabedit<CR>", { silent = true })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
